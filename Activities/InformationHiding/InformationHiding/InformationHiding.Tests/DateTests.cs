be76745cf6c908563c2df1c44b8636c8fd78cc90
Doug Durham <ddurham@dontpaniclabs.com>
2019-04-11 21:38:05 -0500

    Added test project and tests stubs

---
+++ Activities/InformationHiding/InformationHiding/InformationHiding.Tests/DateTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InformationHiding.Tests
+{
+    [TestClass]
+    public class DateTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Activities/InformationHiding/InformationHiding/InformationHiding.Tests/MathTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InformationHiding.Tests
+{
+    [TestClass]
+    public class MathTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
